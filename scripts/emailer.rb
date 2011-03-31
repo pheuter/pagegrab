6613c95ce56cf7bf043b6733001041b54942795b
Mark Fayngersh <phunny.phacts@gmail.com>
2011-03-31 14:52:36 +0000

    script changes

--- scripts/emailer.rb
+++ scripts/emailer.rb
@@ -8,6 +8,8 @@ r.keys.each do |user|
     time = Time.now
     name = "/home/ubuntu/images/#{website[/(http:\/\/|www\.)(.+)/,2]}_#{time.to_i}.png"
     name ||= "/home/ubuntu/images/#{website}_#{time.to_i}.png"
-    `xvfb-run phantomjs rasterize.js #{website} #{name}`
+    `xvfb-run -a phantomjs /home/ubuntu/pagegrab/scripts/rasterize.js #{website} #{name}`
+    Pony.mail :to => user, :from => 'page@pagegrab.it', :subject => "PageGrab | #{website} (#{time.strftime('%a %b %d, %I:%M %P')})", :body => "Attached is a 1680x1050 screenshot of #{website} taken at #{time.strftime('%a %b %d, %I:%M %P')}", :attachments => {"#{website}.png" => File.read(name)}
+    puts "Sent to #{user} on #{time.strftime('%a %b %d, %I:%M %P')} the file located at #{name}"
   end
 end
